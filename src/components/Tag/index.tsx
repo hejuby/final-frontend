a81ac5a1d3b44f5483de23d5fcfee4050c32bd21
hejuby <hejuby@gmail.com>
2024-08-16 06:49:06 +0000

    공통 태그(뱃지) 생성

---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,29 @@
+import { createElement } from "react";
+import ms from "@/utils/modifierSelector";
+import styles from "./index.module.scss";
+
+type TagProps = {
+  color?:
+    | "default"
+    | "light-blue"
+    | "light-purple"
+    | "light-gray"
+    | "dark-gray"
+    | "outline--gray";
+  shape?: "squared" | "rounded";
+} & {
+  children: React.ReactNode;
+};
+
+const cn = ms(styles, "tag");
+
+const Tag = ({ children, color = "default", shape = "squared" }: TagProps) =>
+  createElement(
+    "div",
+    {
+      className: cn(`--color-${color}`, `--shape-${shape}`),
+    },
+    children,
+  );
+
+export default Tag;
