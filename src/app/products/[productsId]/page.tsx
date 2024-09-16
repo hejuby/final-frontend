a3b50b28f9893c1045b97250344bd26e4af5d129
hejuby <hejuby@gmail.com>
2024-09-16 06:54:26 +0000

    fix: 에러수정

--- src/app/products/[productsId]/page.tsx
+++ src/app/products/[productsId]/page.tsx
@@ -1,54 +1,21 @@
-"use client";
-
-import { useParams } from "next/navigation";
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { ICampaignDetails } from "@/@types/campaignItems";
-import Loading from "@/app/Loading";
-import DeatilComponent from "../page";
+import axios from "axios";
+import NotFound from "@/components/NotFound";
+import Page from "../page";
 
-const Product = () => {
-  const { productsId } = useParams();
-  const [campaignData, setCampaignData] = useState<ICampaignDetails | null>(
-    null,
-  );
-  const [isLoading, setIsLoading] = useState(false);
+const ProductPage = async ({ params }: { params: { productsId: string } }) => {
+  const { productsId } = params;
 
-  // 캠페인 상세 데이터 호출
-  useEffect(() => {
-    const getProductData = async () => {
-      try {
-        const response = await axios.get(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/api/campaigns/${productsId}`,
-        );
-        setCampaignData(response.data);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    if (productsId) {
-      getProductData();
-    }
-  }, [productsId]);
-
-  if (isLoading) {
-    return (
-      <div>
-        <Loading />
-      </div>
+  try {
+    const response = await axios.get<ICampaignDetails>(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/campaigns/${productsId}`,
     );
-  }
+    const campaignData = response.data;
 
-  if (!campaignData) {
-    return null;
+    return <Page campaignData={campaignData} />;
+  } catch (error) {
+    return <NotFound />;
   }
-
-  return (
-    <div>
-      <DeatilComponent campaignData={campaignData} />
-    </div>
-  );
 };
 
-export default Product;
+export default ProductPage;
